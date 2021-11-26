8cce033af22adbd9e52bb090668a8d6a2315abc6
j.kunciene <j.kunciene@gmail.com>
2021-11-26 11:12:16 +0200

    isspausdinu tas ir priority

--- js.js
+++ js.js
@@ -2,6 +2,7 @@
 
 const task=document.getElementById('task');
 const priority=document.getElementById('priority');
+const myTable=document.querySelector('tbody');
 
 const myButton=document.getElementById('myButton');
 
@@ -11,6 +12,37 @@ myButton.addEventListener('click', function(e){
 });
 
 function addToDo(){
-    console.log(task.value);
-    console.log(priority.value);
+    if(task.value !== ''){
+        console.log(task.value);
+        console.log(priority.value);
+// kuriu eilute, ja idedu i table body
+      const myRow=document.createElement('tr');
+      myTable.appendChild(myRow);
+// kuriu stulpeli, i ji idedu task reiksme
+      const myCol=document.createElement('td');
+      myCol.textContent=task.value; 
+//   stulpeli appendinu eilutei     
+      myRow.appendChild(myCol);
+
+    //   mano kodas priority reiksmei ideti
+      const myCol1=document.createElement('td');
+      const myPriority=document.createElement('p');
+      myPriority.innerText=priority.value;
+      switch(priority.value){
+            case 'High':
+                myPriority.className="btn btn-warning";                               
+                break;
+            case 'Normal':
+                myPriority.className="btn btn-success";                              
+                break;
+            case 'Low':
+                myPriority.className="btn btn-info";                
+                break;
+      }
+      myCol1.appendChild(myPriority)
+      myRow.appendChild(myCol1);
+
+    } else {
+        alert("Iveskite uzduoti")
+    }
 }
